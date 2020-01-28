358a3e50787f568c83776d678a9a212a3493cb70
anthonyjchriste <achriste@hawaii.edu>
2020-01-27 15:42:39 -1000

    Add language wrappers for protocol.

---
+++ build_protos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if ! [[ -x "$(command -v protoc)" ]]; then
+  echo 'Error: protoc is not installed.' >&2
+  exit 1
+fi
+
+protoc --java_out=./src/java --python_out=./src/python --objc_out=./src/obj-c --js_out=./src/js src/redvox-api-1000.proto
